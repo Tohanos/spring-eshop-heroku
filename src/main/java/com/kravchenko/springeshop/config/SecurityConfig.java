b8bee4c5608450975d3a6d079df9c958d4d4877e
Tohanos <kravchenko_ae@mail.ru>
2021-05-23 17:33:34 +0300

    Homework commit

--- src/main/java/com/kravchenko/springeshop/config/SecurityConfig.java
+++ src/main/java/com/kravchenko/springeshop/config/SecurityConfig.java
@@ -20,7 +20,7 @@ import javax.persistence.Basic;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserService userService;
 
