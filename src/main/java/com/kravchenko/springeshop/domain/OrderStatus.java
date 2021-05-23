8c06c0be8418478782c89a7eb695489ce0a29623
Tohanos <kravchenko_ae@mail.ru>
2021-05-23 11:48:34 +0300

    Classwork commit

---
+++ src/main/java/com/kravchenko/springeshop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.kravchenko.springeshop.domain;
+
+public enum OrderStatus {
+    NEW, APPROVED, CANCELED, PAID, CLOSED
+}
---
+++ src/main/java/com/kravchenko/springeshop/domain/Role.java
@@ -0,0 +1,5 @@
+package com.kravchenko.springeshop.domain;
+
+public enum Role {
+    CLIENT, MANAGER, ADMIN
+}
