04e28f545507573d321557b7af083f664b39dd82
Tohanos <kravchenko_ae@mail.ru>
2021-07-16 22:37:42 +0300

    Commit for test deploying 2

--- src/main/resources/db/migration/V2__add_admin.sql
+++ src/main/resources/db/migration/V2__add_admin.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (id, archive, email, name, password, role, bucket_id)
-VALUES (1, false, 'mail@mail.ru', 'admin', 'pass', 'ADMIN', null);
+VALUES (1, false, 'mail@mail.ru', 'admin', '$2a$10$7ADr06ynxs9I0tF4IF.hdeHgGCqQOcu1kxFKJbjDaTAoBedxyQQae', 'ADMIN', null);
 
 ALTER SEQUENCE user_seq RESTART WITH 2;
